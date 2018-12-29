57ed13af0b5e498bf3d0ac97cb44181127aceefe
Arun Jadhav <arunhjadhav1991@gmail.com>
2018-12-30 01:29:33 +0530

    Basic angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-application';
+
+  no: number;
+
+  constructor() {
+    this.no = 0;
+  }
+
+  increase() {
+    this.no++;
+  }
+
+  decrease() {
+    this.no--;
+  }
+
 }
